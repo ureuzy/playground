2b09ee8794aa2aa05e1f03c7924c7e317c47a3cb
masafumi yamamoto <ureuzy@gmail.com>
2024-04-12 17:48:29 +0900

    update init

--- init.sh
+++ init.sh
@@ -9,9 +9,13 @@ kubectl wait --namespace argocd \
 sleep 10
 
 ARGO_PASS=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
+kubectl port-forward svc/argocd-server -n argocd 8080:80 &
 argocd login localhost:8080 --insecure --username admin --password ${ARGO_PASS} --insecure --grpc-web
 argocd repo add https://github.com/ureuzy/playground.git --project default --grpc-web
+kill `echo $!`
 
 # Install Argo Apps
 kubectl apply -f applications.yaml
+
+echo "Argo admin password: ${ARGO_PASS}"
 
